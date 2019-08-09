9fb717c7730d571a88c4345a229e0e503d6523bd
Joshua DeMaria <34405685+JDeezy5291@users.noreply.github.com>
2019-08-08 23:10:46 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from "react"; //import React object from react package in package.json
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "46a19d39f2aa6cddc978bb47564df32f";
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    pressure: undefined,
+    error: undefined
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const API_CALL = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await API_CALL.json();
+    //checks if city & country value added otherwise return undefined
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        pressure: data.main.pressure,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        pressure: undefined,
+        error: "Please Enter the Values."
+    });
+  }
+}
+  render() {
+    return(
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          pressure={this.state.pressure}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
+};
+
+export default App;
